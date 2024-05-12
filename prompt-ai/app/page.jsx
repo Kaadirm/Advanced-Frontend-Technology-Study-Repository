c597f007a31b1288cfce32482b35db4e6f82e14a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-12 12:54:03 +0300

    homePage start textes and styles

--- prompt-ai/app/page.jsx
+++ prompt-ai/app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <section className="w-full h-screen flex-center flex-col">
+            <h1 className="head_text text-center">Discover $ Share</h1>
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+            <p className="desc text-center">
+                Prompt AI is an open-source AI prompting tool for modern world
+                to discover, create and share creative prompts
+            </p>
+
+            {/* Feed */}
+        </section>
+    );
 };
 
 export default Home;
