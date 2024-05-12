acd46056eefec75861e5b8631359cdecd1da562d
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-12 14:24:11 +0300

    mobile navbar style and functionality added

--- prompt-ai/components/Nav.jsx
+++ prompt-ai/components/Nav.jsx
@@ -5,8 +5,9 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 const Nav = () => {
     const isUserLoggedIn = true;
-
     const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+
     useEffect(() => {
         const setProviders = async () => {
             const response = await getProviders();
@@ -26,7 +27,7 @@ const Nav = () => {
                 />
                 <p className="logo_text">Prompt AI</p>
             </Link>
-            {/* Mobile Navigation */}
+            {/* Desktop Navigation */}
             <div className="sm:flex hidden">
                 {isUserLoggedIn ? (
                     <div className="flex gap-3 md:gap-5">
@@ -49,6 +50,69 @@ const Nav = () => {
                                 alt="Profile"
                             />
                         </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+            {/* Mobile Navigation */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="Profile"
+                            onClick={() =>
+                                setToggleDropdown((preVal) => !preVal)
+                            }
+                        />
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href="/create-prompt"
+                                    className="dropdown_link"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    Create Prompt
+                                </Link>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                        signOut();
+                                    }}
+                                    className="mt-5 w-full black_btn"
+                                >
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
                     </div>
                 ) : (
                     <>
