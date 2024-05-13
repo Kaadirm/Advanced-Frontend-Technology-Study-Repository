9679554b0a7146933a10a5c27d038346528f269a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-13 20:33:10 +0300

    General structure for feed component to list prompts

--- prompt-ai/components/Feed.jsx
+++ prompt-ai/components/Feed.jsx
@@ -1,9 +1,37 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+            {data.map((post) => (
+                <PromptCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    );
+};
 
 const Feed = () => {
+    const [allPosts, setAllPosts] = useState([]);
     const [searchText, setSearchText] = useState('');
     const handleSearchChange = (e) => {};
+
+    const fetchPosts = async () => {
+        const response = await fetch('/api/prompt');
+        const data = await response.json();
+
+        setAllPosts(data);
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
         <section className="feed">
             <form className="relative w-full flex-center">
@@ -16,6 +44,16 @@ const Feed = () => {
                     className="search_input peer"
                 />
             </form>
+
+            {/* All Prompts */}
+            {searchText ? (
+                <PromptCardList
+                    data={searchedResults}
+                    handleTagClick={() => {}}
+                />
+            ) : (
+                <PromptCardList data={allPosts} handleTagClick={() => {}} />
+            )}
         </section>
     );
 };
