2d4e513abe24ea4d2df4ca00de427e193d8c916b
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-13 15:17:45 +0300

    createPrompt functions completed

--- prompt-ai/app/create-prompt/page.jsx
+++ prompt-ai/app/create-prompt/page.jsx
@@ -6,12 +6,34 @@ import { useRouter } from 'next/navigation';
 import Form from '@/components/Form';
 
 const CreatePrompt = () => {
-    const [submitting, setSubmitting] = useState(false);
+    const [submitting, setIsSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
         tag: ''
     });
-    const createPrompt = async (e) => {};
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            });
+
+            if (response.ok) {
+                router.push('/');
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
 
     return (
         <Form
