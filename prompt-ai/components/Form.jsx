ec5b29afbf552829b2b5703719609f87726d4003
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-13 15:10:09 +0300

    tag input with style

--- prompt-ai/components/Form.jsx
+++ prompt-ai/components/Form.jsx
@@ -28,6 +28,23 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
                         required
                     />
                 </label>
+                <label>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">
+                        Tag {` `}
+                        <span className="font-normal">
+                            (#product, #webdevelopment, #idea)
+                        </span>
+                    </span>
+                    <input
+                        value={post.tag}
+                        onChange={(e) =>
+                            setPost({ ...post, tag: e.target.value })
+                        }
+                        className="form_input"
+                        placeholder="#tag"
+                        required
+                    />
+                </label>
             </form>
         </section>
     );
