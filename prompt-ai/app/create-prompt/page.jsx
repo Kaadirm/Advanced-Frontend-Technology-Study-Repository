d0fcc6930cbcb7e9cc13404881b259399e3d2625
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-13 15:14:14 +0300

    called Form component with props

--- prompt-ai/app/create-prompt/page.jsx
+++ prompt-ai/app/create-prompt/page.jsx
@@ -3,11 +3,25 @@
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-
 import Form from '@/components/Form';
 
 const CreatePrompt = () => {
-    return <Form />;
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    });
+    const createPrompt = async (e) => {};
+
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    );
 };
 
 export default CreatePrompt;
