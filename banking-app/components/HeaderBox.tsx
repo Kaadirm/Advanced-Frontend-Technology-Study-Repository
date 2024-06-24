dde86ad05552b167e873a9299f6de9b117781c0b
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-24 11:07:10 +0300

    HeaderBox props and props type added, head part completed

--- banking-app/components/HeaderBox.tsx
+++ banking-app/components/HeaderBox.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const HeaderBox = () => {
-    return <div>HeaderBox</div>;
+const HeaderBox = ({
+    type = 'title',
+    title,
+    user,
+    subtext
+}: HeaderBoxProps) => {
+    return (
+        <div className="header-box">
+            <h1 className="header-box-title">
+                {title}
+                {type === 'greeting' && (
+                    <span className="text-bankGradient">&nbsp;{user}</span>
+                )}
+            </h1>
+        </div>
+    );
 };
 
 export default HeaderBox;
