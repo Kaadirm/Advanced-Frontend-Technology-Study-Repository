6657ff8b3e571ed9a496db2a720a22c4cfa61610
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-25 16:43:34 +0300

    sidebar implementation and active links

--- banking-app/components/Sidebar.tsx
+++ banking-app/components/Sidebar.tsx
@@ -1,13 +1,18 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
 
 const Sidebar = ({ user }: SiderbarProps) => {
+    const pathname = usePathname();
     return (
         <aside className="sidebar">
             <nav className="flex flex-col gap-4">
                 <Link
                     href="/"
-                    className="mb-12 cursor-pointer items-center gap-2"
+                    className="flex mb-12 cursor-pointer items-center gap-2"
                 >
                     <Image
                         src="/icons/logo.svg"
@@ -18,7 +23,41 @@ const Sidebar = ({ user }: SiderbarProps) => {
                     />
                     <h1 className="sidebar-logo">Horizon</h1>
                 </Link>
+                {sidebarLinks.map((item) => {
+                    const isActive =
+                        pathname === item.route ||
+                        pathname.startsWith(`${item.route}/`);
+                    return (
+                        <Link
+                            href={item.route}
+                            key={item.label}
+                            className={cn('sidebar-link', {
+                                'bg-bank-gradient': isActive
+                            })}
+                        >
+                            <div className="relative size-6">
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({
+                                        'brightness-[3] invert-0': isActive
+                                    })}
+                                />
+                            </div>
+                            <p
+                                className={cn('sidebar-label', {
+                                    '!text-white': isActive
+                                })}
+                            >
+                                {item.label}
+                            </p>
+                        </Link>
+                    );
+                })}
+                USER
             </nav>
+            FOOTER
         </aside>
     );
 };
