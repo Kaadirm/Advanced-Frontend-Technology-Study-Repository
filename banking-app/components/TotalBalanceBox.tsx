b52a4b55a97a936eb793ccea32d4318e23ad7c20
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-25 11:01:34 +0300

    accepted props on TotalBalanceBox

--- banking-app/components/TotalBalanceBox.tsx
+++ banking-app/components/TotalBalanceBox.tsx
@@ -1,4 +1,10 @@
-const TotalBalanceBox = () => {
+import React from 'react';
+
+const TotalBalanceBox = ({
+    accounts,
+    totalBanks,
+    totalCurrentBalance
+}: TotalBalanceBoxProps) => {
     return <div>TotalBalanceBox</div>;
 };
 
