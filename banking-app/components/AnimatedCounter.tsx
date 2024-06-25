9f04fdd6cd6d623268ca8040e6ae0efa85bea264
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-25 11:36:55 +0300

    AnimatedCounter w-full style

--- banking-app/components/AnimatedCounter.tsx
+++ banking-app/components/AnimatedCounter.tsx
@@ -2,7 +2,11 @@
 import CountUp from 'react-countup';
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
-    return <CountUp decimals={2} decimal="," prefix="$" end={amount} />;
+    return (
+        <div className="w-full">
+            <CountUp decimals={2} decimal="," prefix="$" end={amount} />
+        </div>
+    );
 };
 
 export default AnimatedCounter;
