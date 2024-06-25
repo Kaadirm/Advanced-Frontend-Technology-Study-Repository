d9fbdf53365499bc56f12355dc9fced3e77e3e61
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-25 15:01:20 +0300

    sidebar head part completed

--- banking-app/components/Sidebar.tsx
+++ banking-app/components/Sidebar.tsx
@@ -1,5 +1,26 @@
-const Sidebar = () => {
-    return <div>Sidebar</div>;
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Sidebar = ({ user }: SiderbarProps) => {
+    return (
+        <aside className="sidebar">
+            <nav className="flex flex-col gap-4">
+                <Link
+                    href="/"
+                    className="mb-12 cursor-pointer items-center gap-2"
+                >
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Horizon logo"
+                        className="size-[24px] max-xl:size-14"
+                    />
+                    <h1 className="sidebar-logo">Horizon</h1>
+                </Link>
+            </nav>
+        </aside>
+    );
 };
 
 export default Sidebar;
