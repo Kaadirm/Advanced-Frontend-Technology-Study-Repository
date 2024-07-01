a59d413b6feaba889e59d02f8bc57f038b0aa58d
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-07-01 19:59:41 +0300

    AuthForm imported and used

--- banking-app/app/(auth)/sign-in/page.tsx
+++ banking-app/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import AuthForm from '@/components/AuthForm';
+
 const signIn = () => {
-    return <div>signIn</div>;
+    return (
+        <section className="flex-center size-full max-sm:px-6">
+            <AuthForm type="sign-in" />
+        </section>
+    );
 };
 
 export default signIn;
