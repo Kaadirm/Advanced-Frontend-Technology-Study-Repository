a4067e2d33fe3dcdfbe691055bd9faf28566f5b1
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-07-03 20:46:01 +0300

    AuthForm header part styled and mostly completed

--- banking-app/components/AuthForm.tsx
+++ banking-app/components/AuthForm.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 const AuthForm = ({ type }: { type: string }) => {
-    return <div>{type}</div>;
+    const [user, setUser] = useState(null);
+    return (
+        <section className="auth-form">
+            <header className="flex flex-col gap-5 md:gap-8">
+                <Link
+                    href="/"
+                    className="flex cursor-pointer items-center gap-1"
+                >
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Horizon logo"
+                    />
+                    <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+                        Horizon
+                    </h1>
+                    <div className="flex flex-col gap-1 md:gap-3">
+                        <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+                            {user
+                                ? 'Link Account'
+                                : type === 'sign-in'
+                                ? 'Sign In'
+                                : 'Sign Up'}
+                            <p className="text-16 font-normal text-gray-600">
+                                {user
+                                    ? 'Link your account to get started'
+                                    : 'Please enter your details'}
+                            </p>
+                        </h1>
+                    </div>
+                </Link>
+            </header>
+        </section>
+    );
 };
 
 export default AuthForm;
