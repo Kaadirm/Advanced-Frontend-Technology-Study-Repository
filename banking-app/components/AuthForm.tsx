991acdbff36feb5abe092d400f175f245b8693b6
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-07-05 21:21:26 +0300

    shadcn/ui React-Hook-Form basic implementation on AuthForm

--- banking-app/components/AuthForm.tsx
+++ banking-app/components/AuthForm.tsx
@@ -1,8 +1,45 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
 
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: 'Username must be at least 2 characters.'
+    })
+});
+
 const AuthForm = ({ type }: { type: string }) => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: ''
+        }
+    });
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    }
+
     const [user, setUser] = useState(null);
     return (
         <section className="auth-form">
@@ -36,6 +73,40 @@ const AuthForm = ({ type }: { type: string }) => {
                     </div>
                 </Link>
             </header>
+            {user ? (
+                <div className="flex flex-col gap-4">{/* Plaidlink */}</div>
+            ) : (
+                <>
+                    {' '}
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="space-y-8"
+                        >
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="shadcn"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is your public display name.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </>
+            )}
         </section>
     );
 };
