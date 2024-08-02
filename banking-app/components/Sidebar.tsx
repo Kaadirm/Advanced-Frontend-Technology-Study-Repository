b2fb888d518a006ffd59fe2abba8ee2fb008e9b6
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-03 00:52:01 +0300

    chore: Add Footer component to Sidebar
    The Footer component is added to the Sidebar component in order to provide a footer section in the banking app.

--- banking-app/components/Sidebar.tsx
+++ banking-app/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { sidebarLinks } from '@/constants';
 import { cn } from '@/lib/utils';
+import Footer from './Footer';
 
 const Sidebar = ({ user }: SiderbarProps) => {
     const pathname = usePathname();
@@ -58,7 +59,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                 })}
                 USER
             </nav>
-            FOOTER
+            <Footer user={user} />
         </aside>
     );
 };
