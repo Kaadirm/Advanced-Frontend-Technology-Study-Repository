e09c130efb18485b73a13d76ce89b44dcd7be84e
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-08 00:10:13 +0300

    Update Footer component to include logout functionality

--- banking-app/components/Footer.tsx
+++ banking-app/components/Footer.tsx
@@ -1,6 +1,16 @@
+import { logoutAccount } from '@/lib/actions/user.action';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Footer = ({ user, type = 'desktop' }: FooterProps) => {
+    const router = useRouter();
+
+    const handleLogOut = async () => {
+        const loggedOut = await logoutAccount();
+
+        if (loggedOut) router.push('/sign-in');
+    };
     return (
         <footer className="footer">
             <div
@@ -24,6 +34,9 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
                 <p className="text-14 truncate font-normal text-gray-600">
                     {user?.email}
                 </p>
+            </div>
+            <div className="footer_image" onClick={handleLogOut}>
+                <Image src="icons/logout.svg" fill alt="jsm" />
             </div>
         </footer>
     );
