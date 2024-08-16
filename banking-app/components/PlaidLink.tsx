7cf86d8b042e5456b7c47cf26e4b3c38000633cc
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-17 00:20:18 +0300

    Update PlaidLink component to use react-plaid-link package and integrate Plaid API

--- banking-app/components/PlaidLink.tsx
+++ banking-app/components/PlaidLink.tsx
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-const PlaidLink = () => {
+import {
+    PlaidLinkOnSuccess,
+    PlaidLinkOptions,
+    usePlaidLink
+} from 'react-plaid-link';
+import { useRouter } from 'next/navigation';
+
+import { createLinkToken } from '@/lib/actions/user.action';
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+    const router = useRouter();
+
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+            const data = await createLinkToken(user);
+            setToken(data?.linkToken);
+        };
+
+        getLinkToken();
+    }, [user]);
+
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(
+        async (public_token: string) => {
+            // await exchangePublicToken({
+            //     publicToken: public_token,
+            //     user
+            // });
+
+            router.push('/');
+        },
+        [user]
+    );
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess
+    };
+
+    const { open, ready } = usePlaidLink(config);
+
     return <div>PlaidLink</div>;
 };
 
