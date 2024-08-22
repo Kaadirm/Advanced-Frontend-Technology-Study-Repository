8622a5e65d19b0a00381ac0b40aa688dfe77c2fb
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-22 13:21:42 +0300

    Update PlaidLink component in AuthForm to include user prop and use variant "primary"

--- banking-app/components/AuthForm.tsx
+++ banking-app/components/AuthForm.tsx
@@ -114,7 +114,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </header>
             {user ? (
                 <div className="flex flex-col gap-4">
-                    <PlaidLink />
+                    <PlaidLink user={user} variant="primary" />
                 </div>
             ) : (
                 <>
