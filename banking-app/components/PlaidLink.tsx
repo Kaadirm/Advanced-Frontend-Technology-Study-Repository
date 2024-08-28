54e268fb164b8e2ac0a8b3235959d8428140039f
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-28 23:11:40 +0300

    Add Dwolla integration for transactions and update PlaidLink component

--- banking-app/components/PlaidLink.tsx
+++ banking-app/components/PlaidLink.tsx
@@ -7,7 +7,10 @@ import {
 } from 'react-plaid-link';
 import { useRouter } from 'next/navigation';
 
-import { createLinkToken } from '@/lib/actions/user.action';
+import {
+    createLinkToken,
+    exchangePublicToken
+} from '@/lib/actions/user.action';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter();
@@ -25,10 +28,10 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
     const onSuccess = useCallback<PlaidLinkOnSuccess>(
         async (public_token: string) => {
-            // await exchangePublicToken({
-            //     publicToken: public_token,
-            //     user
-            // });
+            await exchangePublicToken({
+                publicToken: public_token,
+                user
+            });
 
             router.push('/');
         },
