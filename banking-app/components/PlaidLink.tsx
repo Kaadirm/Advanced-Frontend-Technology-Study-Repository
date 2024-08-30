b6cec8f04a2a4960ff3006aff684874ed51c67e4
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-08-30 23:31:05 +0300

    Update PlaidLink component to include variant "ghost" and add image for connect bank button

--- banking-app/components/PlaidLink.tsx
+++ banking-app/components/PlaidLink.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { Button } from './ui/button';
 
 import {
     PlaidLinkOnSuccess,
@@ -6,11 +7,11 @@ import {
     usePlaidLink
 } from 'react-plaid-link';
 import { useRouter } from 'next/navigation';
-
 import {
     createLinkToken,
     exchangePublicToken
 } from '@/lib/actions/user.action';
+import Image from 'next/image';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter();
@@ -45,7 +46,47 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
     const { open, ready } = usePlaidLink(config);
 
-    return <div>PlaidLink</div>;
+    return (
+        <>
+            {variant === 'primary' ? (
+                <Button
+                    onClick={() => open()}
+                    disabled={!ready}
+                    className="plaidlink-primary"
+                >
+                    Connect bank
+                </Button>
+            ) : variant === 'ghost' ? (
+                <Button
+                    onClick={() => open()}
+                    variant="ghost"
+                    className="plaidlink-ghost"
+                >
+                    <Image
+                        src="/icons/connect-bank.svg"
+                        alt="connect bank"
+                        width={24}
+                        height={24}
+                    />
+                    <p className="hiddenl text-[16px] font-semibold text-black-2 xl:block">
+                        Connect bank
+                    </p>
+                </Button>
+            ) : (
+                <Button onClick={() => open()} className="plaidlink-default">
+                    <Image
+                        src="/icons/connect-bank.svg"
+                        alt="connect bank"
+                        width={24}
+                        height={24}
+                    />
+                    <p className="text-[16px] font-semibold text-black-2">
+                        Connect bank
+                    </p>
+                </Button>
+            )}
+        </>
+    );
 };
 
 export default PlaidLink;
