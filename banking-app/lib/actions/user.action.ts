2326ea7283854143703dbf57207f290e5ac98128
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-09-01 01:28:14 +0300

    Update user.action.ts to add getBanks function

--- banking-app/lib/actions/user.action.ts
+++ banking-app/lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
@@ -214,5 +214,22 @@ export const exchangePublicToken = async ({
         });
     } catch (error) {
         console.error("An error occurred while creating exchanging token:", error);
+    }
+}
+
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const banks = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(banks.documents);
+    } catch (error) {
+        console.log(error)
     }
 }
