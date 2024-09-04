e4670b66f13179d512aa11042776b0e2f17e6f2a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-09-05 01:45:13 +0300

    getLoggedInUser function uses getUserInfo from db

--- banking-app/lib/actions/user.action.ts
+++ banking-app/lib/actions/user.action.ts
@@ -109,8 +109,12 @@ export async function getLoggedInUser() {
     try {
         const { account } = await createSessionClient();
         const result = await account.get();
-        return await account.get();
+
+        const user = await getUserInfo({ userId: result.$id })
+
+        return parseStringify(user);
     } catch (error) {
+        console.log(error)
         return null;
     }
 }
