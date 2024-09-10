15c66a2a07a8460418bd762c8e455f5947f84ffa
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-09-10 14:37:25 +0300

    Refactor Home component to display user's first name instead of full name and dynamically generate data for TotalBalanceBox and RightSidebar

--- banking-app/app/(root)/page.tsx
+++ banking-app/app/(root)/page.tsx
@@ -2,9 +2,19 @@ import HeaderBox from '@/components/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from './../../components/RightSidebar';
 import { getLoggedInUser } from '@/lib/actions/user.action';
+import { getAccounts } from '@/lib/actions/bank.action';
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
     const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({
+        userId: loggedIn.id
+    });
+
+    if (!accounts) return;
+
+    const accountsData = accounts?.data;
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
     return (
         <div className="home">
             <div className="home-content">
@@ -12,21 +22,21 @@ const Home = async () => {
                     <HeaderBox
                         type="greeting"
                         title="Welcome"
-                        user={loggedIn?.name || 'Guest'}
+                        user={loggedIn?.firstName || 'Guest'}
                         subtext="Access and manage your account and transactions efficiently."
                     />
                     <TotalBalanceBox
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1250.35}
+                        accounts={accountsData}
+                        totalBanks={accounts?.totalBanks}
+                        totalCurrentBalance={accounts?.totalCurrentBalance}
                     />
                 </header>
                 RECENT TRANSACTIONS
             </div>
             <RightSidebar
                 user={loggedIn}
-                transaction={[]}
-                banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+                transaction={accounts?.transactions}
+                banks={accountsData?.slice(0, 2)}
             />
         </div>
     );
