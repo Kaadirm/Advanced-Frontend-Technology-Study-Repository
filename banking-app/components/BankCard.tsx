3527f52f56364ef178de089ef8f7108dd402b490
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-10-03 17:33:47 +0300

    Refactor BankCard component to include Copy functionality

--- banking-app/components/BankCard.tsx
+++ banking-app/components/BankCard.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Copy from './Copy';
 
 const BankCard = ({
     account,
@@ -64,6 +65,7 @@ const BankCard = ({
                     className="absolute top-0 left-0"
                 />
             </Link>
+            {showBalance && <Copy title={account?.sharableId} />}
         </div>
     );
 };
